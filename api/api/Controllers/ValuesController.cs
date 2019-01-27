f1055ffa9abaf35c20c4903df11137e76c8160f3
Zhen-Yee <davster2@gmail.com>
2019-01-26 21:58:12 -0500

    speech api google

--- api/api/Controllers/ValuesController.cs
+++ api/api/Controllers/ValuesController.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 using Microsoft.AspNetCore.Mvc;
+using Google.Cloud.Speech.V1;
+using Google.Cloud.Storage.V1;
+using Google.Apis.Auth.OAuth2;
+using NAudio;
 
 namespace api.Controllers
 {
@@ -40,5 +45,115 @@ namespace api.Controllers
         public void Delete(int id)
         {
         }
+
+        [HttpGet("sound")]
+        public string Test()
+        {
+            //var credential = GoogleCredential.FromFile("ConUHacks2019-86e27edeab70.json");
+            //var storage = StorageClient.Create(credential);
+            var speech = SpeechClient.Create();
+            var response = speech.Recognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Flac,
+                //SampleRateHertz = 16000,
+                LanguageCode = "en-US",
+            }, RecognitionAudio.FromFile("testsound.flac"));
+            var i = "";
+            //Console.WriteLine(response.Results);
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    i = alternative.Transcript;
+                    Console.WriteLine(i);
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
+
+            Console.WriteLine("lool");
+
+            return i;
+        }
+
+        [HttpGet("bro")]
+        public Task Mic()
+        {
+            Task x = StreamingMicRecognizeAsync(10);
+            Console.WriteLine("OPLK");
+            return x;
+        }
+
+        public static async Task<object> StreamingMicRecognizeAsync(int seconds)
+        {
+            if (NAudio.Wave.WaveIn.DeviceCount < 1)
+            {
+                Console.WriteLine("No microphone!");
+                return -1;
+            }
+            var speech = SpeechClient.Create();
+            var streamingCall = speech.StreamingRecognize();
+            // Write the initial request with the config.
+            await streamingCall.WriteAsync(
+                new StreamingRecognizeRequest()
+                {
+                    StreamingConfig = new StreamingRecognitionConfig()
+                    {
+                        Config = new RecognitionConfig()
+                        {
+                            Encoding =
+                            RecognitionConfig.Types.AudioEncoding.Linear16,
+                            SampleRateHertz = 16000,
+                            LanguageCode = "en",
+                        },
+                        InterimResults = true,
+                    }
+                });
+            // Print responses as they arrive.
+            Task printResponses = Task.Run(async () =>
+            {
+                while (await streamingCall.ResponseStream.MoveNext(
+                    default(CancellationToken)))
+                {
+                    foreach (var result in streamingCall.ResponseStream
+                        .Current.Results)
+                    {
+                        foreach (var alternative in result.Alternatives)
+                        {
+                            Console.WriteLine(alternative.Transcript);
+                        }
+                    }
+                }
+            });
+            // Read from the microphone and stream to API.
+            object writeLock = new object();
+            bool writeMore = true;
+            var waveIn = new NAudio.Wave.WaveInEvent();
+            waveIn.DeviceNumber = 0;
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(16000, 1);
+            waveIn.DataAvailable +=
+                (object sender, NAudio.Wave.WaveInEventArgs args) =>
+                {
+                    lock (writeLock)
+                    {
+                        if (!writeMore) return;
+                        streamingCall.WriteAsync(
+                            new StreamingRecognizeRequest()
+                            {
+                                AudioContent = Google.Protobuf.ByteString
+                                    .CopyFrom(args.Buffer, 0, args.BytesRecorded)
+                            }).Wait();
+                    }
+                };
+            waveIn.StartRecording();
+            Console.WriteLine("Speak now.");
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+            // Stop recording and shut down.
+            waveIn.StopRecording();
+            lock (writeLock) writeMore = false;
+            await streamingCall.WriteCompleteAsync();
+            await printResponses;
+            return 0;
+        }
+
     }
 }
