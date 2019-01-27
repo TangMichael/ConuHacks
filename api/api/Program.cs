6cdad362c033db05a61d0b7fce403ceb700ee6af
Kevin Luu <kevinateur@hotmail.com>
2019-01-26 21:39:41 -0500

    Server setup

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -14,12 +14,15 @@ namespace api
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseUrls("http://localhost:5000", "http://odin:5000")
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build();
+            host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
     }
 }
