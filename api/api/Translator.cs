5c86e8079f87ba95be486c3cbd4c1f2d70bd3198
Kevin Luu <kevinateur@hotmail.com>
2019-01-26 23:25:40 -0500

    Add translator

---
+++ api/api/Translator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Google.Cloud.Translation.V2;
+
+namespace api
+{
+    public class Translator
+    {
+
+
+        public Translator()
+        { }
+
+            public string translateEnglish(string txt)
+            {
+                    Console.OutputEncoding = System.Text.Encoding.Unicode;
+            TranslationClient client = TranslationClient.Create();
+                var response = client.TranslateText("Bonjour.", "en");
+                return response.TranslatedText;
+            }
+
+        public string translateFrench(string txt)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            TranslationClient client = TranslationClient.Create();
+            var response = client.TranslateText("Hello World.", "fr");
+            return response.TranslatedText;
+        }
+
+
+    
+    }
+}
